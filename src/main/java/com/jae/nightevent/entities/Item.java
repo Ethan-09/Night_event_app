2739fd860a5073edcdb794f6e4ccbb0175c1f9f6
BabyScylla69 <alvarosilvalobos99@gmail.com>
2024-11-21 15:41:10 +0100

    fix item

--- src/main/java/com/jae/nightevent/entities/Item.java
+++ src/main/java/com/jae/nightevent/entities/Item.java
@@ -1,15 +1,18 @@
 package com.jae.nightevent.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "item")
-public class Item {
+public class Item implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
